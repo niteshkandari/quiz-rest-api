f06f578f6478f2c744a4baed15af6fb21354445c
nitesh kandari <nitesh.kandari@azilen.com>
2021-05-05 12:06:27 +0530

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+const question = require ('./question.json');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const data = question;
+
+app.get('/' , (req,res) => {
+  res.send('welcome to the rest api');
+});
+
+app.get('/api/q',(req,res) => {
+  res.send(data);
+})
+
+
+
+const port = process.env.port || 3000;
+app.listen(port , () => console.log(`listening on port ${port}`));
